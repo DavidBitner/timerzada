c3c273f8a7c08a726a4c1985eba66a9e7da82dfc
David <david-bitner@hotmail.com>
2020-09-20 23:13:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,61 @@
+# Em construção
 from tkinter import *
-from time import *
+from time import sleep
 
 
+# Função para selecionar o texto que estiver dentro da caixa de texto das horas
 def horas_entry_callback(event):
     horas_entry.selection_range(0, END)
 
 
+# Função para selecionar o texto que estiver dentro da caixa de texto dos minutos
 def minutos_entry_callback(event):
     minutos_entry.selection_range(0, END)
 
 
+# Função para selecionar o texto que estiver dentro da caixa de texto dos segundos
 def segundos_entry_callback(event):
     segundos_entry.selection_range(0, END)
+
+
+# Função do timer
+def timer():
+    global timer_label
+    global stop_btn
+    horas = int(horas_entry.get())
+    minutos = int(minutos_entry.get())
+    segundos = int(segundos_entry.get())
+
+    while horas > 0 or minutos > 0 or segundos > 0:
+        timer_label.destroy()
+        stop_btn.destroy()
+        timer_label = Label(root, text=f"{horas} : {minutos} : {segundos}", font=("Helvetica", 40))
+        timer_label.pack()
+        timer_label.update()
+        stop_btn = Button(root, text="STOP", height=2, width=20)
+        stop_btn.pack()
+        stop_btn.update()
+        if segundos > 0:
+            segundos -= 1
+        elif segundos == 0:
+            if minutos > 0:
+                minutos -= 1
+                segundos = 59
+            if minutos == 0:
+                if horas > 0:
+                    horas -= 1
+                    minutos = 59
+                    segundos = 59
+        sleep(1)
 
 
 # Main
 root = Tk()
 root.title("Timerzada")
 root.geometry("500x500+200+200")
+
+timer_label = Label(root)
+stop_btn = Button(root)
 
 # Frame
 frame = LabelFrame(root, padx=5, pady=5)
@@ -52,9 +90,7 @@ minutos_label.grid(row=2, column=1)
 segundos_label.grid(row=2, column=2)
 
 # Botões do timer
-start_btn = Button(frame, text="START", height=2, width=20)
+start_btn = Button(frame, text="START", height=2, width=20, command=timer)
 start_btn.grid(row=3, column=0, columnspan=3, padx=20, pady=20)
-
-
 
 root.mainloop()
